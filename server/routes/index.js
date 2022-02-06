ddf62a638072474726c3a816bbc53bbbfd0edf9e
KovtunenkoAnton <kovshik@mail.ru>
2022-02-06 20:02:32 +0300

    Connect to server done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const DB = [
+  { title: 'first', id: uuidv4(), text: 'some text' }, 
+  { title: 'second', id: uuidv4(), text: 'some text' },
+  { title: 'third', id: uuidv4(), text: 'some text' }
+]
+
+router.route('/')
+  .get(function (req, res) {
+    res.json(DB)
+  })
 
 module.exports = router;
